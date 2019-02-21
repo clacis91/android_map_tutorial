80cd6b4888f982d49b28ef69a949431fbafb749b
wjnam <clacis91@gmail.com>
2019-02-21 17:58:44 +0900

    Initial commit

---
+++ app/src/main/java/com/example/user/placeapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.user.placeapp;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.mapLayout, new MapsActivity()).commit();
+    }
+}
